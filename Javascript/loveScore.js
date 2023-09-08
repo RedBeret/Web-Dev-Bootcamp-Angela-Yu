59d80378ef2553f5a1bb12f97a1af0b0bbd28c70
RedBeret <espisteven5@gmail.com>
2023-09-07 21:26:00 -0700

    loveScore js uploaded

---
+++ Javascript/loveScore.js
@@ -0,0 +1,10 @@
+prompt("what is your name?");
+prompt("What is their name?");
+
+var loveScore = Math.random();
+loveScore = loveScore * 100;
+loveScore = Math.floor(loveScore) + 1;
+console.log(loveScore);
+alert("Your love score is " + loveScore + "%");
+
+ 
