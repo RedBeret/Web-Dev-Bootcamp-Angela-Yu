78912a04ea3b86f4f9dea7388f5a8dd2f9af65a1
RedBeret <espisteven5@gmail.com>
2023-09-11 20:48:46 -0700

    guestname

--- Javascript/guestList.js
+++ Javascript/guestList.js
@@ -1,7 +1,15 @@
+// Original list of guests with proper casing
 var guestList = ["Joe", "Bob", "Rob", "Sob"];
 
-var guestName = prompt("what is your name?");
+// Prompt user for their name and immediately convert to lowercase
+var guestName = prompt("What is your name?").toLowerCase();
 
-if (guestList.includes(guestName)){
-    alert("welcome");
-} else alert("you are not welcome");
+// Convert first letter to uppercase and the rest to lowercase
+var formattedGuestName = guestName.charAt(0).toUpperCase() + guestName.slice(1);
+
+// Check if the formatted name exists in the guest list
+if (guestList.includes(formattedGuestName)) {
+    alert("Welcome, " + formattedGuestName + "!");
+} else {
+    alert("I'm sorry, you are not on the guest list.");
+}
