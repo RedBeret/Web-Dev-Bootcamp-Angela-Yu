b280c7a9993a3dfac10b6429697e100b79bd2df9
RedBeret <espisteven5@gmail.com>
2023-09-11 20:37:00 -0700

    guestlist

---
+++ Javascript/guestList.js
@@ -0,0 +1,7 @@
+var guestList = ["Joe", "Bob", "Rob", "Sob"];
+
+var guestName = prompt("what is your name?");
+
+if (guestList.includes(guestName)){
+    alert("welcome");
+} else alert("you are not welcome");
